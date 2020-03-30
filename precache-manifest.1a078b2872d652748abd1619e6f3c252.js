a43315d59f1fc290a4ad01d696249e18314ce4d1
GreySup <47871929+GreySup@users.noreply.github.com>
2020-03-30 22:13:51 +0200

    Add files via upload

---
+++ precache-manifest.1a078b2872d652748abd1619e6f3c252.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772786205f3899a20c04e6ef516d4d37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb7916a976870967df2c",
+    "url": "/static/css/main.88417db8.chunk.css"
+  },
+  {
+    "revision": "546383baab7597018e03",
+    "url": "/static/js/2.74e9d0ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74e9d0ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb7916a976870967df2c",
+    "url": "/static/js/main.5c03fa72.chunk.js"
+  },
+  {
+    "revision": "f952a7f6dc638f5a732c",
+    "url": "/static/js/runtime-main.34179c8c.js"
+  },
+  {
+    "revision": "a97b8022cdb88f5cb08df8d68bb9aec3",
+    "url": "/static/media/FoundersGroteskLight.a97b8022.woff2"
+  },
+  {
+    "revision": "eedb987a866f58010316565f6248edc8",
+    "url": "/static/media/FoundersGroteskMedium.eedb987a.woff2"
+  },
+  {
+    "revision": "98c0fe231453addc6d2cbd541775cef4",
+    "url": "/static/media/NeueHaasUnicaRegular.98c0fe23.woff2"
+  },
+  {
+    "revision": "725d15b71cfa056868de7356e6126701",
+    "url": "/static/media/Platform-Regular-Web.725d15b7.woff"
+  }
+]);
